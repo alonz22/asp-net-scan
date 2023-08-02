207d5aca23221acfd00c6d65e5c83923a18003c6
Alon Zur <72250573+alonz22@users.noreply.github.com>
2023-08-02 17:08:38 +0300

    Update Scan-File-Upload-Mechanism.ps1
    scanning .aspx.cs and .cs in addition to .aspx

--- Scan-File-Upload-Mechanism.ps1
+++ Scan-File-Upload-Mechanism.ps1
@@ -1,4 +1,4 @@
-﻿function Check-FileUploadVulnerabilities {
+function Check-FileUploadVulnerabilities {
     param($file)
 
     $content = Get-Content -Path $file
@@ -55,13 +55,17 @@ if (-not (Test-Path $path -PathType Container)) {
     exit
 }
 
-# Get all .aspx files in the given path recursively
+# Get all .aspx, .aspx.cs, and .cs files in the given path recursively
 $aspxFiles = Get-ChildItem -Path $path -Filter "*.aspx" -Recurse
+$aspxCSFiles = Get-ChildItem -Path $path -Filter "*.aspx.cs" -Recurse
+$csFiles = Get-ChildItem -Path $path -Filter "*.cs" -Recurse
 
-# Perform checks on each .aspx file
+$allFiles = $aspxFiles + $aspxCSFiles + $csFiles  # Combine all the file arrays
+
 $matchesFound = $false
 $matchCounter = 0  # Initialize total match counter
-foreach ($file in $aspxFiles) {
+
+foreach ($file in $allFiles) {
     $matches, $count = Check-FileUploadVulnerabilities $file.FullName
     if ($matches) {
         $matchesFound = $true

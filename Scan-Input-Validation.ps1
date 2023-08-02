ce6de779bed8f6a9fb7790d13ddf39fbed73da11
Alon Zur <72250573+alonz22@users.noreply.github.com>
2023-08-02 17:02:56 +0300

    Update Scan-Input-Validation.ps1
    scanning .aspx.cs and .cs in addition to .aspx

--- Scan-Input-Validation.ps1
+++ Scan-Input-Validation.ps1
@@ -1,4 +1,4 @@
-﻿function Check-InputValidation {
+function Check-InputValidation {
     param($file)
 
     $content = Get-Content -Path $file
@@ -56,13 +56,17 @@ if (-not (Test-Path $path -PathType Container)) {
     exit
 }
 
-# Get all .aspx files in the given path recursively
+# Get all .aspx, .aspx.cs, and .cs files in the given path recursively
 $aspxFiles = Get-ChildItem -Path $path -Filter "*.aspx" -Recurse
+$aspxCSFiles = Get-ChildItem -Path $path -Filter "*.aspx.cs" -Recurse
+$csFiles = Get-ChildItem -Path $path -Filter "*.cs" -Recurse
+
+$allFiles = $aspxFiles + $aspxCSFiles + $csFiles  # Combine all the file arrays
 
 $matchesFound = $false
 $matchCounter = 0  # Initialize total match counter
 
-foreach ($file in $aspxFiles) {
+foreach ($file in $allFiles) {
     $matches, $count = Check-InputValidation $file.FullName
     if ($matches) {
         $matchesFound = $true
